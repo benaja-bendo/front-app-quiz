b7cbd827aa80ecff338331390585a1c8cbb990eb
Mourtada <mourtadam21@gmail.com>
2024-05-15 14:50:05 +0200

    modifs

--- src/pages/GenerateQuiz.tsx
+++ src/pages/GenerateQuiz.tsx
@@ -2,23 +2,28 @@ import { FC } from 'react';
 import { Form, Navigate, useActionData } from 'react-router-dom';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { useCurrentUser } from '@/hook/use-current-user';
-import httpService from '@/services/HttpService';
-import configRoutes from '@/utils/config-routes';
-
+import { useNavigate } from 'react-router-dom';
 
 export const GenerateQuiz: FC = () => {
-    const { currentUser } = useCurrentUser();
+    const navigate = useNavigate();
     const data = useActionData() as { status?: number };
-    if (data?.status !== undefined) return <Navigate to={'/'} />;
+    if (data?.status !== undefined) return <Navigate to={'generate-quiz/start'} />;
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const title = formData.get('title') as string;
+        const level = formData.get('level') as string;
+        navigate(`/start?title=${title}&level=${level}`);
+    };
 
     return (
         <div className="flex justify-center items-center h-screen">
-            <Form action="/generate-quiz/start" method="post" encType={"multipart/form-data"} className="border border-gray-300 rounded-lg p-6">
+            <Form onSubmit={handleSubmit} className="border border-gray-300 rounded-lg p-6">
                 <p className="text-xl font-semibold mb-4">Générer un Quiz</p>
                 <p className="text-gray-600 mb-4">Générer un quiz aléatoire pour vous ou pour vos amis</p>
                 <div className="grid grid-cols-1 gap-4">
-                    <Input id="skill" name="title" placeholder="Titre" />
+                    <Input id="skill" name="title" placeholder="Titre" required/>
                     <select name="level" className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300">
                         <option value="facile">Facile</option>
                         <option value="moyen">Moyen</option>
@@ -34,18 +39,3 @@ export const GenerateQuiz: FC = () => {
     );
 };
 
-export const GenerateQuizAction = async ({ request }: ActionFunctionArgs) => {
-    const body = await request.formData();
-    const res = await httpService.post(configRoutes.quizzes.getAll, body, {
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    });
-    console.log(res);
-    return {
-        status: 200,
-        body: {
-            message: 'Quiz ajouté avec succès'
-        }
-    };
-};
